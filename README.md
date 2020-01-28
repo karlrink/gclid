1b3759401381a47cc1c56eeddc60b8b9a8ed53ec
karl <karl.rink@nationsinfocorp.com>
2020-01-28 11:47:29 -0800

    dump.gclid.py >out.csv

--- README.md
+++ README.md
@@ -20,7 +20,6 @@ $ apt-get install -y python-mysql.connector
 ```
 
 ##################################################################################################
-
 # dbUser...
 ```
 # mysql 8
@@ -33,5 +32,31 @@ mysql> create user 'gclid'@'localhost' IDENTIFIED WITH mysql_native_password BY 
 mysql> grant 'gclid_role' to 'gclid'@'localhost';
 
 ```
+
+##################################################################################################
+# output
+```
+### INSTRUCTIONS ###,,,,
+"# IMPORTANT: Remember to set the TimeZone value in the ""parameters"" row and/or 
+in your Conversion Time column",,,,
+# For instructions on how to set your timezones visit http://goo.gl/T1C5Ov,,,,
+,,,,
+### TEMPLATE ###,,,,
+Parameters:TimeZone=America/Los_Angeles
+Google_Click_ID,Conversion_Name,Conversion_Time,Conversion_Value,Conversion_Currency
+```
+
+Conversion Time: the date and time that the conversion occurred, example,
+```
+yyyy-MM-ddTHH:mm:ss zzzz | "2012-08-14T13:00:00 America/Los_Angeles"
+
+```
+
+example...
+```
+Cj0KCQiAiNnuBRD3ARIsAM8KmlsWutPbDFd5lmMUtyDOuQrEv5IlJKgnSFqqV1qu4kTizFf54XtBynEaArErEALw_wcB,15-day ELTV adjustment,2012-08-14 13:00:00 America/Los_Angeles,partner.vertical_type,USD
+```
+
+
 
 
--- dump.gclid.py
+++ dump.gclid.py
@@ -2,7 +2,6 @@
 
 import sys
 sys.dont_write_bytecode = True
-#import os
 
 try:
     import mysql.connector
@@ -41,27 +40,37 @@ except mysql.connector.Error as e:
     print(str(e))
     sys.exit(1)
 
+sql =  "SELECT main.tbl_customer.gclid, nic_billing.order.order_date "
+sql += "FROM main.tbl_customer INNER JOIN nic_billing.order "
+sql += "ON nic_billing.order.id_customer = main.tbl_customer.CUSTOMER_ID "
+sql += "WHERE main.tbl_customer.gclid IS NOT NULL "
+sql += "AND nic_billing.order.order_status = 'success' "
+sql += "AND nic_billing.order.order_date >= (CURDATE() - INTERVAL 14 DAY) "
+sql += "ORDER BY nic_billing.order.order_date ASC "
+#sql += "LIMIT 5"
 
 cursor = cnx.cursor(buffered=True)
-
 try:
-    sql = "show status;"
     cursor.execute(sql)
     if cursor.rowcount > 0:
-        show_status = cursor.fetchall()
+        get_results = cursor.fetchall()
     else:
-        show_status = ''
+        get_results = ''
 
 except Exception as e:
     print(str(e))
+    sys.exit(1)
 
 cursor.close()
 cnx.close()
 
-#work with the data...
-# we have show_status fetchall()/list
-for row in show_status:
-    print(row)
+# work with the data...
+# we have get_results fetchall()/list
+#for row in get_results:
+#    print(row)
 
+for (gclid, order_date) in get_results:
+    #print(row)
+    print("{},15-day ELTV adjustment,{:%Y-%m-%d %H:%M:%S} America/Los_Angeles,partner.vertical_type,USD".format(gclid, order_date))
 
-#basic setup done... continue this tomorrow...
+sys.exit(0)
