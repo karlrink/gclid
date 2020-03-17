1e4e744ff9d4730b258475202cac390556bd2705
karl <karl.rink@nationsinfocorp.com>
2020-03-17 09:51:11 -0700

    dump.gclid.brand.py

---
+++ dump.gclid.brand.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+
+import sys
+sys.dont_write_bytecode = True
+
+def get_results(brand_id):
+
+	try:
+	    import mysql.connector
+	except ImportError as e:
+	    print(str(e))
+	    print('Try...')
+	    print('    redhat install: yum install mysql-connector-python')
+	    print('    debian install: apt-get install python-mysql.connector')
+	    sys.exit(1)
+
+	try:
+	    import config
+	except ImportError as e:
+	    print('Missing config.py: ' + str(e))
+	    sys.exit(1)
+	dbSocket = config.gclid['dbSocket']
+	dbUser = config.gclid['dbUser']
+	dbPass = config.gclid['dbPass']
+
+	try:
+	    config = {
+		'user': dbUser,
+		'password': dbPass,
+		'unix_socket': dbSocket,
+		'database': '',
+		'raise_on_warnings': True,
+		'auth_plugin': 'mysql_native_password',
+	    }
+	except Exception as e:
+	    print(str(e)) 
+	    sys.exit(1)
+
+	try:
+	    cnx = mysql.connector.connect(**config)
+	except mysql.connector.Error as e:
+	    print(str(e))
+	    sys.exit(1)
+
+
+	#brand_id = '3'
+
+	sql =  "SELECT main.tbl_customer.gclid, nic_billing.order.order_date "
+	sql += "FROM main.tbl_customer INNER JOIN nic_billing.order "
+	sql += "ON nic_billing.order.id_customer = main.tbl_customer.CUSTOMER_ID "
+	sql += "WHERE main.tbl_customer.BRAND_ID = '" + str(brand_id) + "' "
+	sql += "AND main.tbl_customer.gclid IS NOT NULL "
+	sql += "AND nic_billing.order.order_status = 'success' "
+	sql += "AND nic_billing.order.order_date >= (CURDATE() - INTERVAL 31 DAY) "
+	sql += "ORDER BY nic_billing.order.order_date ASC "
+	#sql += "LIMIT 5"
+
+	cursor = cnx.cursor(buffered=True)
+	try:
+	    cursor.execute(sql)
+	    if cursor.rowcount > 0:
+		get_results = cursor.fetchall()
+	    else:
+		get_results = ''
+
+	except Exception as e:
+	    print(str(e))
+	    sys.exit(1)
+
+	cursor.close()
+	cnx.close()
+
+	# work with the data...
+	# we have get_results fetchall()/list
+	#for row in get_results:
+	#    print(row)
+	return get_results
+
+if __name__ == '__main__':
+    if sys.argv[1:]:
+        brand_id = sys.argv[1]
+    else:
+        print('Usage: ' + sys.argv[0] + ' brand_id')
+        sys.exit(0)
+
+    get_results = get_results(brand_id)
+
+    if get_results:
+        print("Google Click ID,Conversion Name,Conversion Time,Conversion Value,Conversion Currency")
+        for (gclid, order_date) in get_results:
+            #print(row)
+            print("{},15-day ELTV adjustment,{:%Y-%m-%d %H:%M:%S} America/Los_Angeles,partner.vertical_type,USD".format(gclid, order_date))
+
+
